bd64cfa1b5a96a79c7b4f6ae22235aab0990487c
ubaidul rehman <ubaidulrehman92@.github.com>
2020-09-01 21:30:32 +0100

    production build

---
+++ build/precache-manifest.93d67e454697ddb0c9453e24743115bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118d6738957a8a7050a792b77cf5efb2",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "1cf44316034a9bae8f7b",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/my-app/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cf44316034a9bae8f7b",
+    "url": "/my-app/static/js/main.ca7e66ca.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
